c014b9e70e8c0d2e153e91a9b26bb3f3919a1133
ChengnanHu <hcnhcn012@126.com>
2020-10-20 20:03:58 +0800

    add array problems

--- array/RemoveElement/RemoveElement/main.cpp
+++ array/RemoveElement/RemoveElement/main.cpp
@@ -6,9 +6,56 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int removeElement(vector<int>& nums, int val);
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(int argc, const char * argv[])
+{
+    vector<int> v = {0,1,2,2,3,0,4,2};
+    int len = removeElement(v, 3);
+    for (int i=0; i<v.size(); i++)
+    {
+        cout << v[i] << ' ';
+    }
+    cout << "\nAfter removing, len is " << len << endl;
     return 0;
+}
+
+int removeElement(vector<int>& nums, int val)
+{
+    if (nums.size() == 0)
+        return 0;
+    int *p1, *p2;
+    int len = 0;
+    p1 = &nums[0];
+    p2 = &nums[nums.size()-1];
+    // 交换头部开始的val值和尾部开始的非val值
+    while (p1 != p2)
+    {
+        if (*p1 == val)
+        {
+            if (*p2 != val)
+            {
+                int temp = *p1;
+                *p1 = *p2;
+                *p2 = temp;
+            }
+            else
+            {
+                p2--;
+                continue;
+            }
+        }
+        else
+            p1++;
+    }
+    // remove操作后计数
+    for (; len<nums.size(); len++)
+    {
+        if (nums[len] == val)
+            break;
+    }
+    return len;
 }
