bcbfe891a740e30b7b373e85d60c4d8c90c209aa
ChengnanHu <hcnhcn012@126.com>
2021-01-09 20:11:05 +0800

    add result output

--- DynamicProgramming/ZeroOneKnapsack/ZeroOneKnapsack/main.cpp
+++ DynamicProgramming/ZeroOneKnapsack/ZeroOneKnapsack/main.cpp
@@ -10,19 +10,25 @@
 
 using namespace std;
 
-int knapsack(int capacity, vector<int> &items);
+int knapsack(int capacity, vector<int> &items, vector<bool> &result);
 
 int main(int argc, const char * argv[])
 {
     vector<int> stuff = {2,2,4,6,3};
     const int capacity = 9;
-    int max_weight = knapsack(capacity, stuff);
+    vector<bool> result = vector<bool>(stuff.size(), false);
+    int max_weight = knapsack(capacity, stuff, result);
     cout << "Max weight: " << max_weight;
+    cout << endl;
+    for (int i=0; i<result.size(); i++)
+    {
+        cout << result[i] << ' ';
+    }
     cout << endl;
     return 0;
 }
 
-int knapsack(int capacity, vector<int> &items)
+int knapsack(int capacity, vector<int> &items, vector<bool> &result)
 {
     /* init states matrix */
     /* matrix row: item's weight */
@@ -40,20 +46,34 @@ int knapsack(int capacity, vector<int> &items)
         }
         
         /* pack when free space is enough */
-        for (int j=0; j<=capacity; j++)
+        for (int k=0; k<=capacity; k++)
         {
-            if (states[i-1][j] == true && j+items[i]<=capacity)
-                states[i][j+items[i]] = true;
+            if (states[i-1][k] == true && k+items[i]<=capacity)
+                states[i][k+items[i]] = true;
         }
     }
     
     /* get max value */
-    int j = capacity;
+    int p = capacity;
     int last_item_idx = int(items.size()) - 1;
-    for (; j>=0; j--)
+    for (; p>=0; p--)
     {
-        if (states[last_item_idx][j] == 1)
+        if (states[last_item_idx][p] == true)
             break;
     }
-    return j;
+    
+    /* fill-in the results vector */
+    int q = p;
+    int r = last_item_idx;
+    while (r>=0 && q>=0)
+    {
+        if (q-items[r] >= 0 && states[r-1][q-items[r]] == true)
+        {
+            result[r] = true;
+            q = q - items[r];
+        }
+        --r;
+    }
+    
+    return p;
 }
